b9db3e0ccf9ef0431ce04d451da40b9e6fdd2233
chandanglowtech <50921978+chandanglowtech@users.noreply.github.com>
2024-05-04 13:39:21 +0530

    DATATYPES

---
+++ dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all javascript code as a newer version of js 
+
+alert("hello") // we are using node js this is posible in browser 
+
+console.log("hello 1"); console.log("hello 2"); // use semi colon for this dont do this because code readiablity is bad
+
+
+let name = "Chandan" // string
+let age = 30 //number
+
+isLoggedIn = true //boolien
+
+// null stand alone value / empty value
+
+// undefined = value not defined
+
+// symbol = unique 
+
+
+// object
+console.log(undefined); // type will be undefined 
+console.log(null); // type will be object
