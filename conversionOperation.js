2a1432949b8af4da45b7bf2c78807d016362d9cf
chandanglowtech <50921978+chandanglowtech@users.noreply.github.com>
2024-05-04 15:16:59 +0530

    datatypes

---
+++ conversionOperation.js
@@ -0,0 +1,45 @@
+let score = "30a"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score); // capital N converts to number  NaN not a number
+console.log(valueInNumber);
+console.log(typeof(valueInNumber));
+
+
+let score1 = null // output will be 0 
+let valueInNumber1 = Number(score1);
+
+
+
+// 33 = 33
+// "33abc"  = NaN
+// null = 0
+// undefined = NaN
+// true = 1
+// false = 0
+
+let isLoggedIn = 1
+console.log(Boolean(isLoggedIn)); //true
+
+let isLoggedIn1 = ""
+console.log(Boolean(isLoggedIn1)); //false
+
+
+let isLoggedIn2 = "chandan"
+console.log(Boolean(isLoggedIn2)); //true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+//************operations ************************
+//base math operation + - / % * **
+
+
+
--- dataTypes.js
+++ dataTypes.js
@@ -1,14 +1,17 @@
 "use strict"; // treat all javascript code as a newer version of js 
 
-alert("hello") // we are using node js this is posible in browser 
+//alert("hello") 
+// we are using node js this is posible in browser 
 
 console.log("hello 1"); console.log("hello 2"); // use semi colon for this dont do this because code readiablity is bad
 
-
+/*
 let name = "Chandan" // string
 let age = 30 //number
 
 isLoggedIn = true //boolien
+
+*/
 
 // null stand alone value / empty value
 
@@ -20,3 +23,15 @@ isLoggedIn = true //boolien
 // object
 console.log(undefined); // type will be undefined 
 console.log(null); // type will be object
+
+
+// primitive datatypes 
+// 7 types : string , number, boolien, null, undefined, symbol, BigInt (larger numbers) 
+
+// non primitive or referance type
+// Array, Objects, functions
+
+const id = Symbol("22");
+const anotherId = Symbol("22")
+
+console.log(id === anotherId); // output will be false because symbol is used for unique identification
